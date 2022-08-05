accd5671e4374bb54c78fbb90a64e6caa33836d0
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-08-05 21:01:11 +0530

    Cleaned up `unload` and `reload` commands

--- bot.py
+++ bot.py
@@ -149,35 +149,27 @@ async def load(ctx, *, arg1):
 
 @bot.command()
 async def unload(ctx, *, arg1):
-    if ctx.message.author.id in oid:
-        pass
-    else:
+    if ctx.message.author.id not in oid:
         await ctx.reply(f"You can\'t use this command")
-        print(f"[Cog] {ctx.author} returned an error: User Missing Permission.")
-        return
+        return print(f"[Cog] {ctx.author} returned an error: User Missing Permission.")
     try:
         bot.unload_extension(f'cogs.{arg1}')
         await ctx.send("Unloaded Cog")
         print(f"[Cog] {ctx.author} unloaded cog.")
-        return
     except Exception as e:
         await ctx.send(e)
         print(f"[Cog] An unexpected error has occurred: {e}")
         
 @bot.command()
 async def reload(ctx, *, arg1):
-    if ctx.message.author.id in oid:
-        pass
-    else:
+    if ctx.message.author.id not in oid:
         await ctx.reply(f"You can\'t use this command")
-        print(f"[Cog] {ctx.author} returned an error: User Missing Permission.")
-        return
+        return print(f"[Cog] {ctx.author} returned an error: User Missing Permission.")
     try:
         bot.unload_extension(f'cogs.{arg1}')
         bot.load_extension(f'cogs.{arg1}')
         await ctx.send("Reloaded Cog")
         print(f"[log] {ctx.author} reloaded cog.")
-        return
     except Exception as e:
         await ctx.send(e)
         print(f"[Cog] An unexpected error has occurred: {e}")
